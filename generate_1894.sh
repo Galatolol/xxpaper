b7387277d84fc78ec080c745933854d174af81cb
Galatolol <galatolol@protonmail.com>
2023-11-30 23:34:46 +0100

    Restore 1894

--- generate_1894.sh
+++ generate_1894.sh
@@ -6,6 +6,8 @@ TOKEN_OBJECT_NAME='XXP_token_objects.xxp'
 TOKEN_OBJECT_NAME_ALT='cXXP_token_objects.xxp'
 CATALOG='1894'
 
+mkdir -p $CATALOG
+
 xxpaper make assets $SOURCE_FILE -p A4 $CATALOG/papers.pdf
 qpdf $CATALOG/papers.pdf --pages . 1-10 -- --replace-input
 mv $TOKEN_OBJECT_NAME_ALT $TOKEN_OBJECT_NAME
