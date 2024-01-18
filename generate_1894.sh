c433d4e2481127f4a7a9304a581bac4a93cbb41c
Galatolol <galatolol@protonmail.com>
2024-01-18 23:34:27 +0100

    Updates

--- generate_1894.sh
+++ generate_1894.sh
@@ -6,9 +6,32 @@ TOKEN_OBJECT_NAME='XXP_token_objects.xxp'
 TOKEN_OBJECT_NAME_ALT='cXXP_token_objects.xxp'
 CATALOG='1894'
 
-xxpaper make assets $SOURCE_FILE -p A4 $CATALOG/papers.pdf
-qpdf $CATALOG/papers.pdf --pages . 1-10 -- --replace-input
+mkdir -p $CATALOG
+
+# qpdf --replace-input - modify the file in place
+
+#xxpaper make assets $SOURCE_FILE -p A4 $CATALOG/papers.pdf
+#qpdf $CATALOG/papers.pdf --pages . 1-10 -- --replace-input
+#mv $TOKEN_OBJECT_NAME_ALT $TOKEN_OBJECT_NAME
+#xxpaper make assets $SOURCE_FILE_TOKENS -c -p A4 $CATALOG/tokens.pdf
+#qpdf $CATALOG/tokens.pdf --pages . 11 -- --replace-input
+#mv $TOKEN_OBJECT_NAME $TOKEN_OBJECT_NAME_ALT
+
+
+###
+
+
+xxpaper make assets $SOURCE_FILE -p A4 $CATALOG/papers-A4.pdf
+qpdf --pages $CATALOG/papers-A4.pdf 1-r2 -- $CATALOG/papers-A4.pdf --replace-input
 mv $TOKEN_OBJECT_NAME_ALT $TOKEN_OBJECT_NAME
-xxpaper make assets $SOURCE_FILE_TOKENS -c -p A4 $CATALOG/tokens.pdf
-qpdf $CATALOG/tokens.pdf --pages . 11 -- --replace-input
+xxpaper make assets $SOURCE_FILE_TOKENS -c -p A4 $CATALOG/markers-A4.pdf
+qpdf $CATALOG/markers-A4.pdf --pages . 11 -- --replace-input
+mv $TOKEN_OBJECT_NAME $TOKEN_OBJECT_NAME_ALT
+
+
+xxpaper make assets $SOURCE_FILE -p letter $CATALOG/papers-letter.pdf
+qpdf --pages $CATALOG/papers-letter.pdf 1-r2 -- $CATALOG/papers-letter.pdf --replace-input
+mv $TOKEN_OBJECT_NAME_ALT $TOKEN_OBJECT_NAME
+xxpaper make assets $SOURCE_FILE_TOKENS -c -p letter $CATALOG/markers-letter.pdf
+qpdf $CATALOG/markers-letter.pdf --pages . 11 -- --replace-input
 mv $TOKEN_OBJECT_NAME $TOKEN_OBJECT_NAME_ALT
